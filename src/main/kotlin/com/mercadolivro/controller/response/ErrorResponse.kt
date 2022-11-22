0aef58c6412c566a10ca4571f22683060d76b0fc
fabio-elioterio <fabioelioterio.77@gmail.com>
2022-11-22 16:57:15 -0300

    model error classes

---
+++ src/main/kotlin/com/mercadolivro/controller/response/ErrorResponse.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.controller.response
+
+data class ErrorResponse(
+
+        var httpCode: Int,
+        var message: String,
+        var internalCode: String,
+        var errors: List<FieldErrorResponse>?
+)

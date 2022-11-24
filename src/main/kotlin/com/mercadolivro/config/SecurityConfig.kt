086a23ad7cfb9eb15f47c612623a4c08f0b1aa2e
fabio-elioterio <fabioelioterio.77@gmail.com>
2022-11-24 11:40:33 -0300

    permite customer without authentication

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,10 +2,30 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig() : WebSecurityConfigurerAdapter() {
+
+    private val PUBLIC_POST_MATCHERS = arrayOf(
+            "/customer"
+    )
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable()
+        http.authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+
+        // Aqui estamos dizendo que as requisções que chegarem são independentes, não tem nada aver com as anteriores
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
